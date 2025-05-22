ab34bfd3de41e027b8c8b50278ebd38d00490e66
itc-n25005 <n25005@std.it-college.ac.jp>
2025-05-22 06:45:04 +0000

    P69 第４章　チャレンジ問題終了

---
+++ Chapter04/challenge4-03.py
@@ -0,0 +1,9 @@
+g = 1
+h = 3
+i = 2
+
+def number(a, b, c, y=7, z=5): 
+    result = int(a + b + c + y + z)
+    print(result)
+
+number(g, h, i)
---
+++ Chapter04/challenge4-05.py
@@ -0,0 +1,10 @@
+def decimal(a):
+ try:
+  print(a)
+  print(float(a))
+ except ValueError:
+  print(7)
+#a = str(2)
+#a = str("あ")
+a =str("")
+decimal(a)
